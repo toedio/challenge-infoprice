04d7930a1ccba2703ad2d88865d1eafed82f6d0b
Victor Ferreira da Silva <victor.silva@routeasy.com.br>
2017-05-09 21:11:38 -0300

    finish frontend

--- src/main/resources/public/modules/routes.js
+++ src/main/resources/public/modules/routes.js
@@ -8,10 +8,17 @@
             .when('/', {
                 templateUrl : 'modules/user/views/user.home.client.view.html',
                 controller: "UserController"
-            })          
+            })
+            .when('/users/create', {
+                templateUrl : 'modules/user/views/user.create.client.view.html',
+                controller: "UserController"
+            })
+            .when('/users/edit/:userId', {
+                templateUrl : 'modules/user/views/user.edit.client.view.html',
+                controller: "UserController"
+            })           
             .otherwise({
                 redirectTo: '/'
             });
-            $locationProvider.html5Mode(true);
         });
 })();
--- src/main/resources/public/modules/user/controller/user.client.controller.js
+++ src/main/resources/public/modules/user/controller/user.client.controller.js
@@ -5,8 +5,83 @@
         .module('testjava.user')
         .controller('UserController', UserController);
 
-    UserController.inject = ['$scope', 'Cities'];
-    function UserController($scope, Cities) {
-        $scope.cities = Cities.query();
+    UserController.inject = ['$scope', '$location', '$routeParams', 'Cities', 'Users'];
+    function UserController($scope, $location, $routeParams, Cities, Users) {
+
+        $scope.initList = function() {            
+            $scope.users = Users.query();
+        };
+
+        $scope.initFormUser = function() {
+            $scope.cities = Cities.query();
+        };
+
+        $scope.initCreation = function() {
+            $scope.user = { address: [ { } ] };
+        };
+
+        $scope.initEdition = function() {
+            $scope.user = Users.get({userId: $routeParams.userId});
+        };
+
+        $scope.goToCreate = function() {
+            $location.path('users/create');
+        };
+
+        $scope.goToList = function() {
+        	$location.path('/');
+        };
+
+        $scope.goToEdit = function(user) {
+            $location.path('users/edit/' + user.userId);
+        };
+
+        $scope.formatAddress = function(address) {
+            return  (address.route ? address.route + ', ' : '' )+
+                    (address.streetNumber ? address.streetNumber + ', ': '') +
+                    (address.neighborhood ? address.neighborhood + ', ': '') +
+                    (address.city.cityName ? address.city.cityName + ', ' : '' )+
+                    (address.city.state ? address.city.state + ', ' : '' )+
+                    (address.city.country ? address.city.country + ' ' : '');
+        };
+
+        $scope.addAddress = function(user) {
+            if (!user.address)
+                user.address = [];
+            user.address.push({});
+        };
+
+        $scope.removeAddress = function(list, idx) {
+        	return list.splice(idx, 1);
+        };        
+
+        $scope.create = function(user) {
+            var user = new Users(user);
+            user.$save(function(response) {
+                $location.path('/');
+            }, function(err) {
+                alert('Failed to create user. Try again later');
+            });
+        };
+
+        $scope.remove = function(user, idx) {
+            if (confirm('Are you sure you want to delete ?')) {
+                var user = new Users(user);
+                user.$remove(function (response) {
+                    $scope.users.splice(idx, 1);
+                }, function(err) {
+                    alert('Failed to remove');
+                })
+            }
+        };
+        
+        $scope.update = function(user) {
+            var user = new Users(user);
+            user.$update(function(response) {
+                $location.path('/');
+            }, function(err) {
+                alert('Failed to update user. Try again later');
+            });
+        };        
     }
 })();
